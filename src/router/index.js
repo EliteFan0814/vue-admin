1ecbf6a9004b1b70e3ebd4ac35063c019d5b6a15
elite fan <fan552426811@outlook.com>
2019-07-01 18:26:47 +0800

    周一下班

--- src/router/index.js
+++ src/router/index.js
@@ -4,31 +4,36 @@ import store from '@/store/index'
 import Cookie from 'js-cookie'
 
 import { commonRoutes } from '@/mock/routers.js'
+import routers from '../store/modules/routers';
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
-      component: () => import('@/pages/login/login.vue')
-    }
-  ]
+let router = new Router({
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'HelloWorld',
+  //     component: HelloWorld
+  //   },
+  //   {
+  //     path: '/login',
+  //     component: () => import('@/pages/login/login.vue')
+  //   }
+  // ]
 })
 
 // 根据是否有 cookie 来判断是否进行初始化
 if(Cookie.getJSON('accountData') && Cookie.getJSON('accountData').isLogin){
   // 根据 cookie 设置用户登录状态
-  store.dispatch('storeUser/setUser',Cookie.getJSON('accountData')).then(() => {
+  store.dispatch('storeUser/actSetUser',Cookie.getJSON('accountData')).then(() => {
     let _roles = store.getters.userInfo._roles
     // 根据 roles 权限生成可访问的路由表
     store.dispatch('storeRouters/getRoutes',_roles).then(() => {
-
+      routers.addRoutes(store.getters.addRoutes)
     })
   })
+}else{
+  routers.addRoutes([commonRoutes()])
 }
+
+export default router
